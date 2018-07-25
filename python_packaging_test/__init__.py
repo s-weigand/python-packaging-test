18d536f45dccdd86d7bc55e80c71670eb39bb588
Sebastian Weigand <s.weigand.phy@gmail.com>
2018-07-25 14:16:55 +0200

    Bump version: 0.0.8 → 0.0.9

--- python_packaging_test/__init__.py
+++ python_packaging_test/__init__.py
@@ -3,7 +3,7 @@
 """Top-level package for python-packaging-test."""
 
 from python_packaging_test.python_packaging_test import demo_function  # noqa:
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 __author__ = """Sebastian Weigand"""
 __email__ = 's.weigand.phy@gmail.com'
