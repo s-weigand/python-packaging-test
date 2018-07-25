06380d8e9dbd18a4acdcbf55bf5fced0735fa9f0
Sebastian Weigand <s.weigand.phy@gmail.com>
2018-07-25 14:11:57 +0200

    making clean

--- python_packaging_test/__init__.py
+++ python_packaging_test/__init__.py
@@ -3,7 +3,7 @@
 """Top-level package for python-packaging-test."""
 
 from python_packaging_test.python_packaging_test import demo_function  # noqa:
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 __author__ = """Sebastian Weigand"""
 __email__ = 's.weigand.phy@gmail.com'
