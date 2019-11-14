e9b863feb142ef80f4cbe10e2291f6b30ea84c9f
s-weigand <s.weigand.phy@gmail.com>
2019-11-14 15:22:36 +0100

    fixed sorting issue

--- python_packaging_test/__init__.py
+++ python_packaging_test/__init__.py
@@ -2,11 +2,11 @@
 
 """Top-level package for python-packaging-test."""
 
+# testing __init__ imports
+import numpy as np  # noqa:
 from python_packaging_test.python_packaging_test import demo_function  # noqa:
+
 __version__ = '0.0.10'
 
 __author__ = """Sebastian Weigand"""
 __email__ = 's.weigand.phy@gmail.com'
-
-# testing __init__ imports
-import numpy as np  # noqa:
