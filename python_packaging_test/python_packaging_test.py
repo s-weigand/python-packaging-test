e29abb64cc7b490bcff9ec7097c45baac2778bbd
s-weigand <s.weigand.phy@gmail.com>
2019-11-20 05:04:05 +0100

    added test functions with branches and used codecov action again

--- python_packaging_test/python_packaging_test.py
+++ python_packaging_test/python_packaging_test.py
@@ -2,6 +2,15 @@
 
 """Main module."""
 
+GLOBAL_VAR = 1
+
 
 def demo_function():
     return "demo"
+
+
+def brach_function(var):
+    if var:
+        return True
+    else:
+        return False
--- tests/test_python_packaging_test.py
+++ tests/test_python_packaging_test.py
@@ -5,7 +5,18 @@
 
 
 from python_packaging_test import demo_function
+from python_packaging_test.python_packaging_test import brach_function, GLOBAL_VAR
+
+
+def test_GLOBAL_VAR():
+    assert GLOBAL_VAR == 1
 
 
 def test_demo_function():
     assert demo_function() == "demo"
+
+
+def test_brach_function():
+    assert brach_function(True) == True
+    assert brach_function(False) == False
+
